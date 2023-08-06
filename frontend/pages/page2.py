1acb567043c36a0d5670f60a7ac9a68b0120cb07
Fran3x <franex@onet.eu>
2023-08-06 16:23:29 +0200

    plottable for all players

--- frontend/pages/page2.py
+++ frontend/pages/page2.py
@@ -7,45 +7,50 @@ def load_image(path):
     image = Image.open(path)
     return image
 
-def page2():
-    # st.title("Fantasy Premier League point predictions")
-    
-    plot_dir = "D:/PulpitE/FPL_ML/plottable/"
-    plottable_images = os.listdir(plot_dir)
-    
+@st.cache(allow_output_mutation=True)
+def get_number_of_plots(plottable_images):
     plots_all = sum('predictions_plot_All_' in s for s in plottable_images)
     plots_gk = sum('predictions_plot_GK_' in s for s in plottable_images)
     plots_def = sum('predictions_plot_DEF_' in s for s in plottable_images)
     plots_mid = sum('predictions_plot_MID_' in s for s in plottable_images)
     plots_fwd = sum('predictions_plot_FWD_' in s for s in plottable_images)
     
-    print(plots_all, plots_gk, plots_def, plots_mid, plots_fwd)
-    
-    for i in plottable_images:
-        print(i)
-    
-    NUMBER_OF_PLOTS = 1
-    
+    return plots_all, plots_gk, plots_def, plots_mid, plots_fwd
+
+@st.cache(allow_output_mutation=True)
+def get_images(plots_all, plots_gk, plots_def, plots_mid, plots_fwd):
     PIL_images_all = [load_image('D:/PulpitE/FPL_ML/plottable/predictions_plot_All_' + str(i) + ".png") for i in range(1, plots_all + 1)]
     PIL_images_GK = [load_image('D:/PulpitE/FPL_ML/plottable/predictions_plot_GK_' + str(i) + ".png") for i in range(1, plots_gk + 1)]
     PIL_images_DEF = [load_image('D:/PulpitE/FPL_ML/plottable/predictions_plot_DEF_' + str(i) + ".png") for i in range(1, plots_def + 1)]
     PIL_images_MID = [load_image('D:/PulpitE/FPL_ML/plottable/predictions_plot_MID_' + str(i) + ".png") for i in range(1, plots_mid + 1)]
     PIL_images_FWD = [load_image('D:/PulpitE/FPL_ML/plottable/predictions_plot_FWD_' + str(i) + ".png") for i in range(1, plots_fwd + 1)]
+    
+    return PIL_images_all, PIL_images_GK, PIL_images_DEF, PIL_images_MID, PIL_images_FWD
+
+
+def page2():
+    # st.title("Fantasy Premier League point predictions")
+    
+    plot_dir = "D:/PulpitE/FPL_ML/plottable/"
+    plottable_images = os.listdir(plot_dir)
+    plots_all, plots_gk, plots_def, plots_mid, plots_fwd = get_number_of_plots(plottable_images)
+    
+    PIL_images_all, PIL_images_GK, PIL_images_DEF, PIL_images_MID, PIL_images_FWD = get_images(plots_all, plots_gk, plots_def, plots_mid, plots_fwd)
+    
+    def get_total_pages(current_position):
+        if current_position == "All positions":
+            return plots_all
+        elif current_position == "Goalkeepers":
+            return plots_gk
+        elif current_position == "Defenders":
+            return plots_def
+        elif current_position == "Midfielders":
+            return plots_mid
+        return plots_fwd
 
     content = st.container()
     bottom_menu = st.columns((4, 1, 1))
     
-    def get_total_pages(current_position):
-        if current_position == "All positions":
-            return NUMBER_OF_PLOTS
-        elif current_position == "Goalkeepers":
-            return NUMBER_OF_PLOTS
-        elif current_position == "Defenders":
-            return NUMBER_OF_PLOTS
-        elif current_position == "Midfielders":
-            return NUMBER_OF_PLOTS
-        return NUMBER_OF_PLOTS
-        
     with bottom_menu[1]:
         current_position = st.selectbox(
             'Choose position',
@@ -53,9 +58,7 @@ def page2():
         )
     with bottom_menu[2]:
         total_pages = (
-            
             get_total_pages(current_position)
-            
         )
         current_page = st.number_input(
             "Page", min_value=1, max_value=total_pages, step=1
