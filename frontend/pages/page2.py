79a7a25c76c4428e750e2aa2c11ab0943e685d8f
Fran3x <franex@onet.eu>
2023-08-06 14:42:01 +0200

    position choice

--- frontend/pages/page2.py
+++ frontend/pages/page2.py
@@ -10,12 +10,15 @@ def load_image(path):
 def page2():
     # st.title("Fantasy Premier League point predictions")
     
-    NUMBER_OF_PLOTS = 3
+    NUMBER_OF_PLOTS = 1
     
-    PIL_images = [load_image('D:/PulpitE/FPL_ML/plottable/predictions_plot_' + str(i) + ".png") for i in range(1, NUMBER_OF_PLOTS + 1)]
-
-    PIL_image = load_image('D:/PulpitE/FPL_ML/plottable/predictions_plot_1.png')
-    PIL_image2 = load_image('D:/PulpitE/FPL_ML/plottable/predictions_plot_2.png')
+    # PIL_images = [load_image('D:/PulpitE/FPL_ML/plottable/predictions_plot_' + str(i) + ".png") for i in range(1, NUMBER_OF_PLOTS + 1)]
+    
+    PIL_images_all = [load_image('D:/PulpitE/FPL_ML/plottable/predictions_plot_All_' + str(i) + ".png") for i in range(1, NUMBER_OF_PLOTS + 1)]
+    PIL_images_GK = [load_image('D:/PulpitE/FPL_ML/plottable/predictions_plot_GK_' + str(i) + ".png") for i in range(1, NUMBER_OF_PLOTS + 1)]
+    PIL_images_DEF = [load_image('D:/PulpitE/FPL_ML/plottable/predictions_plot_DEF_' + str(i) + ".png") for i in range(1, NUMBER_OF_PLOTS + 1)]
+    PIL_images_MID = [load_image('D:/PulpitE/FPL_ML/plottable/predictions_plot_MID_' + str(i) + ".png") for i in range(1, NUMBER_OF_PLOTS + 1)]
+    PIL_images_FWD = [load_image('D:/PulpitE/FPL_ML/plottable/predictions_plot_FWD_' + str(i) + ".png") for i in range(1, NUMBER_OF_PLOTS + 1)]
 
     content = st.container()
     bottom_menu = st.columns((4, 1, 1))
@@ -28,13 +31,21 @@ def page2():
             "Page", min_value=1, max_value=total_pages, step=1
         )
     with bottom_menu[1]:
-        option = st.selectbox(
+        current_position = st.selectbox(
             'Choose position',
             ('All positions', 'Goalkeepers', 'Defenders', 'Midfielders', 'Forwards')
         )
     with bottom_menu[0]:
         st.markdown(f"Page **{current_page}** of **{total_pages}** ")
-        
-    content.image(PIL_images[current_page - 1], caption='Point predictions for next gameweek')
-        
+    
+    if current_position == "All positions":
+        content.image(PIL_images_all[current_page - 1], caption='Point predictions for next gameweek')
+    elif current_position == "Goalkeepers":
+        content.image(PIL_images_GK[current_page - 1], caption='Point predictions for next gameweek')
+    elif current_position == "Defenders":
+        content.image(PIL_images_DEF[current_page - 1], caption='Point predictions for next gameweek')
+    elif current_position == "Midfielders":
+        content.image(PIL_images_MID[current_page - 1], caption='Point predictions for next gameweek')
+    else:
+        content.image(PIL_images_FWD[current_page - 1], caption='Point predictions for next gameweek')
     
