e97e0e3a8ca94ef90a30a40c5f2a1ae3e10502b5
Fran3x <franex@onet.eu>
2023-08-07 02:15:55 +0200

    changed title and favicon

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,11 @@ from pages.page1 import page1
 from pages.page2 import page2
 from pages.page3 import page3
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+    layout="wide",
+    page_title = "FPL predictor",
+    page_icon = "https://freepngimg.com/download/premier_league/32207-5-premier-league-file.png"
+)
 
 page_names_to_funcs = {
     "Overview": page1,
@@ -18,10 +22,10 @@ with st.sidebar:
         "Menu", ["Overview", 'Predictions', 'How it works?'], 
         icons=['house', 'graph-up', 'question'],
         menu_icon="list", 
-         styles={
-            "container": {"background-color": "#f0f2f6"},
-            "nav-link-selected": {"background-color": "#ff5d5d"},
-         }
+            styles={
+                "container": {"background-color": "#f0f2f6"},
+                "nav-link-selected": {"background-color": "#ff5d5d"},
+            }
         )
 
 page_names_to_funcs[selected]()
