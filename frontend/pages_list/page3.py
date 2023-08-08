dfcc810207e343c77c2d4d9f636fdd773a97b6c2
Fran3x <franex@onet.eu>
2023-08-08 15:30:42 +0200

    file selector hidden

--- frontend/pages_list/page3.py
+++ frontend/pages_list/page3.py
@@ -53,11 +53,3 @@ def page3():
         pagination.dataframe(data=pages[current_page - 1], 
             #  use_container_width=True
         )
-
-    def file_selector(folder_path='.'):
-        filenames = os.listdir(folder_path)
-        selected_filename = st.selectbox('Select a file', filenames)
-        return os.path.join(folder_path, selected_filename)
-
-    filename = file_selector()
-    st.write('You selected `%s`' % filename)
