d6d11da308677392dd090ac1b1fb921419471b04
Fran3x <franex@onet.eu>
2023-08-14 01:15:57 +0200

    page3 bug fix

--- frontend/pages_list/page3.py
+++ frontend/pages_list/page3.py
@@ -2,13 +2,6 @@ import streamlit as st
 import pandas as pd
 import os
 import time
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
 
 def page3():
     st.title("Model architecture")
-
-    with Diagram("Web Service", show=False):
-        ELB("lb") >> EC2("web") >> RDS("userdb")
