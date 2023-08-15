1a292925c21f3faa632db5bb7be54da6599feff6
Fran3x <franex@onet.eu>
2023-08-15 03:46:44 +0200

    price added

--- frontend/pages_list/page3.py
+++ frontend/pages_list/page3.py
@@ -2,6 +2,18 @@ import streamlit as st
 import pandas as pd
 import os
 import time
+from PIL import Image
+import requests
+import urllib.request
+
+@st.cache(allow_output_mutation=True)
+def load_image(url):
+    urllib.request.urlretrieve("https://resources.premierleague.com/premierleague/photos/players/250x250/p172850.png", "image.png")
+    image = Image.open("image.png")
+    return image
 
 def page3():
     st.title("Model architecture")
+    
+    image = load_image("https://resources.premierleague.com/premierleague/photos/players/250x250/p172850.png")
+    st.image(image, caption='Ben Chilwell', width=200)
