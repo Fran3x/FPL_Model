645d6fb108b09210249f1a39821fb9bb884555a4
Fran3x <franex@onet.eu>
2023-08-15 17:20:33 +0200

    best eleven png

--- frontend/pages_list/page3.py
+++ frontend/pages_list/page3.py
@@ -8,12 +8,13 @@ import urllib.request
 
 @st.cache(allow_output_mutation=True)
 def load_image(url):
-    urllib.request.urlretrieve("https://resources.premierleague.com/premierleague/photos/players/250x250/p172850.png", "image.png")
-    image = Image.open("image.png")
+    # urllib.request.urlretrieve("https://resources.premierleague.com/premierleague/photos/players/250x250/p172850.png", "image.png")
+    # image = Image.open("image.png")
+    image = Image.open("plottable/predictions_plot_best_eleven.png")
     return image
 
 def page3():
-    st.title("Model architecture")
+    st.title("Best eleven")
     
     image = load_image("https://resources.premierleague.com/premierleague/photos/players/250x250/p172850.png")
-    st.image(image, caption='Ben Chilwell', width=200)
+    st.image(image, caption="Best eleven for next gameweek")
