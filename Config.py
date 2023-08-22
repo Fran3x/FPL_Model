11321501b587f9b1c031923955fee1bba6fa23e3
Fran3x <franex@onet.eu>
2023-08-23 01:16:43 +0200

    config updated with features

--- Config.py
+++ Config.py
@@ -1,8 +1,47 @@
 CURRENT_SEASON = "2023-24"
 CURRENT_SEASON_FULL = "2023-2024"
-
 NEXT_GAMEWEEK = 3
 
 # training parameters
 CUT_OFF_GAMEWEEK = 31
 SEASON_TO_PREDICT = "2022-23"
+
+# features
+FEATURES = ['Was_home',
+ 'Rating_difference',
+ 'Value',
+ 'Transfers_balance',
+ 'Avg_FPL_points',
+ 'Min_2',
+ 'Gls_2',
+ 'Ast_2',
+ 'xG_2',
+ 'xA_2',
+ 'Team_CS_2',
+ 'Team_score_2',
+ 'Opp_score_2',
+ 'Team_result_2',
+ 'FPL_points_2',
+ 'Min_4',
+ 'Gls_4',
+ 'Ast_4',
+ 'xG_4',
+ 'xA_4',
+ 'Team_CS_4',
+ 'Team_score_4',
+ 'Opp_score_4',
+ 'Team_result_4',
+ 'FPL_points_4',
+ 'Min_30',
+ 'Gls_30',
+ 'Ast_30',
+ 'xG_30',
+ 'xA_30',
+ 'Team_CS_30',
+ 'Team_score_30',
+ 'Opp_score_30',
+ 'Team_result_30',
+ 'FPL_points_30',
+ 'DEF',
+ 'FWD',
+ 'MID']
