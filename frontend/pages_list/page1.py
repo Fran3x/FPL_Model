8253ad370c02e828a0042a6bc2ce0704ab0fab38
Fran3x <franex@onet.eu>
2023-08-24 16:50:54 +0200

    main page desc updated

--- frontend/pages_list/page1.py
+++ frontend/pages_list/page1.py
@@ -10,19 +10,19 @@ def page1():
 
                 ## Your Ultimate Tool for Winning Your Fantasy League
 
-                Are you ready to dominate your fantasy league? Look no further! Our FPL Predictor uses advanced machine learning algorithms to forecast player performance in the upcoming gameweeks of Premier League. Our model analyzes player statistics, historical data, match fixtures, and more to give you accurate predictions for player points.
+                Are you ready to dominate your fantasy league? Look no further! FPL Predictor uses advanced machine learning algorithms to forecast player performance in the upcoming gameweeks of Premier League. Model analyzes player statistics, historical data, match fixtures, and more to give you accurate predictions for player points.
 
 
                 ## Why FPL Predictor?
 
-                - 📊 **Data-Driven**: Our predictions are based on a wealth of data, ensuring accuracy and reliability.
-                - 🧠 **Machine Learning Magic**: Our advanced algorithms adapt and learn from past performances to deliver the best predictions.
+                - 📊 **Data-Driven**: Predictions are based on a wealth of data, ensuring accuracy and reliability.
+                - 🧠 **Machine Learning Magic**: Advanced algorithms adapt and learn from past performances to deliver the best predictions.
                 - ⏰ **Real-Time Updates**: Get real-time updates on predicted points, injuries, and more to stay ahead of the game.
                 - 🏆 **Dominate Your League**: Make strategic decisions and lead your fantasy league with confidence.
 
                 ## Get Started Today!
 
-                Join the ranks of successful fantasy managers who trust FPL Predictor. Whether you're a seasoned manager or a newcomer, our predictions will give you the edge you need to conquer your fantasy league.
+                Join the ranks of successful fantasy managers who trust FPL Predictor. Whether you're a seasoned manager or a newcomer, predictions will give you the edge you need to conquer your fantasy league.
             """)
         
     with col2:
