02a944ebf7d8c53d668f0b5e96b64907a2382804
Fran3x <franex@onet.eu>
2023-08-30 14:14:21 +0200

    fbref team urls

--- Config.py
+++ Config.py
@@ -53,6 +53,30 @@ current_teams_fpl = [
         'Wolves'
     ]
 
+
+fbref_team_urls = [
+        "https://fbref.com/en/squads/b8fd03ef/Manchester-City-Stats",
+        "https://fbref.com/en/squads/7c21e445/West-Ham-United-Stats",
+        "https://fbref.com/en/squads/361ca564/Tottenham-Hotspur-Stats",
+        "https://fbref.com/en/squads/822bd0ba/Liverpool-Stats",
+        "https://fbref.com/en/squads/18bb7c10/Arsenal-Stats",
+        "https://fbref.com/en/squads/d07537b9/Brighton-and-Hove-Albion-Stats",
+        "https://fbref.com/en/squads/8602292d/Aston-Villa-Stats",
+        "https://fbref.com/en/squads/19538871/Manchester-United-Stats",
+        "https://fbref.com/en/squads/cd051869/Brentford-Stats",
+        "https://fbref.com/en/squads/cff3d9bb/Chelsea-Stats",
+        "https://fbref.com/en/squads/47c64c55/Crystal-Palace-Stats",
+        "https://fbref.com/en/squads/fd962109/Fulham-Stats",
+        "https://fbref.com/en/squads/b2b47a98/Newcastle-United-Stats",
+        "https://fbref.com/en/squads/e4a775cb/Nottingham-Forest-Stats",
+        "https://fbref.com/en/squads/8cec06e1/Wolverhampton-Wanderers-Stats",
+        "https://fbref.com/en/squads/4ba7cbea/Bournemouth-Stats",
+        "https://fbref.com/en/squads/1df6b87e/Sheffield-United-Stats",
+        "https://fbref.com/en/squads/943e8050/Burnley-Stats",
+        "https://fbref.com/en/squads/e297cd13/Luton-Town-Stats",
+        "https://fbref.com/en/squads/d3fd31cc/Everton-Stats"
+    ]
+
 def current_teams_to_fpl(team):
     if team == "Manchester City":
         return 'Man City'
